bb1a1f465329f2147fe4d3212ea27d58b446990e
Michael Malocha <mmalocha13@gmail.com>
2018-02-11 12:35:10 -0800

    query attempt

--- lambda/es.py
+++ lambda/es.py
@@ -9,33 +9,87 @@ import conf
 
 session = boto3.session.Session()
 awsauth = AWS4Auth(
-        conf.KEY,
-        conf.SECRET,
-        conf.REGION,
-        'es'
-        )
+    conf.KEY,
+    conf.SECRET,
+    conf.REGION,
+    'es'
+    )
 es = Elasticsearch(
-        hosts=[{'host': conf.HOST, 'port':443}],
-        http_auth=awsauth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection,
-        )
+    hosts=[{'host': conf.HOST, 'port':443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+    )
 
 def post_to_es(body, id):
-    _type = conf.TYPE
-    _index = conf.INDEX
-    es.index(index=_index, doc_type=_type, id=id, body=body)
+    es.index(
+        index=conf.INDEX,
+        doc_type=conf.TYPE,
+        id=id,
+        body=body
+        )
 
-def get_from_es():
-    index = conf.INDEX
-    _type = conf.TYPE
+
+def get__all():
     res = es.search(
-            index=index,
-            doc_type=_type,
-            body={"query": {"match_all": {}}},
-            )
+        index=conf.INDEX,
+        doc_type=conf.TYPE,
+        body={"query": {"match_all": {}}},
+        )
     return res['hits']['hits']
+
+
+def search_es(species=None, lifestage=None, location=None):
+    query = {
+        "query": {
+            "bool": {
+                "must": []
+                }
+            }
+        }
+    if species:
+        query['query']['filtered'] = {
+            'filter': {
+                'terms': {
+                    'supported_species': [
+                        species,
+                        ]
+                    }
+                }
+            }
+    if lifestage:
+        query['query']['bool'].setdefault('should', [])
+        query['query']['bool']['should'].append({
+            "term": {
+                "fish_lifestage": lifestage
+                }
+            })
+        query['query']['bool']['should'].append({
+            "term": {
+                "fish_lifestage": ""
+                }
+            })
+        query['query']['bool']['should'].append({
+            "term": {
+                "fish_lifestage": "All"
+                }
+            })
+    if location:
+        query['query']['bool']['must'].append(
+            {'match': {
+                'supplier_location': location
+                }
+            })
+
+    print(query)
+    res = es.search(
+        index=conf.INDEX,
+        doc_type=conf.TYPE,
+        body=query,
+        )
+    return res
+
 
 def delete_all():
     return es.delete_by_query(index='_all', body='')
